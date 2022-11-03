3a683ac0c898319c51214a2aa6c5b50a989ea8a5
Александра <aleksandra.gulida@gmail.com>
2022-11-03 20:47:41 +0300

    hello

---
+++ forexam2.py
@@ -0,0 +1,115 @@
+import os
+#os.mkdir('c:\\Users\\Александра\\Documents\\zadachi')
+
+import os
+#os.rmdir('c:\\Users\\Александра\\Documents\\zadachi')
+
+import os
+#os.mkdir('c:\\Users\\Александра\\Documents\\task_1.txt')
+#os.rename('c:\\Users\\Александра\\Documents\\task_1.txt','c:\\Users\\Александра\\Documents\\renamefile.txt')
+
+#
+# a = 'hellomynameis'
+# for i in a:
+#     b = a.count(i)
+#     dict= {}
+#     dict[i]=b
+#     print(dict)
+
+
+#x = dict(zip([1,2,3],['one', 'two', 'three']))
+#print(x)
+
+# a = [1,2,3,4,5]
+# b = [1,3,5]
+# c = []
+# for i in a:
+#     if i in b:
+#         c.append(i)
+# print(len(c))
+
+# a = input('введите десять чисел через запятую')
+# b = set()
+# for i in a:
+#     print(b.add(a))
+
+# import random
+# a = [random.randint(1,50) for i in range(50)]
+# b = tuple(a)
+# print(b)
+
+
+# dict1 = {'one': 1,
+#          'two': 2,
+#          'three':3,
+#          'four':4}
+# result = 1
+# for i in dict1:
+#     result = result * dict1[i]
+# print(result)
+
+
+# a = input('vvedite 10 chelih chisel')
+# b = list(a)
+# b.sort()
+# print(b[0:2])
+
+# a = [75, 2, 4, 2, 87, 9, 3]
+# print('spisok:',a)
+# a.sort()
+# print('2 naimenshih chisla:',a[0:2])
+
+# a = [-1, 5, 36, -81, 25, 60, -12]
+# result = 0
+# for i in a:
+#     if i % 2 == 0 and i > 0:
+#         result = result + i
+# print(result)
+
+# print('ряд Фибоначчи')
+# a = int(input('сколько элементов вывести?'))
+# f1 = 1
+# f2 = 1
+# f1 = f2
+# print(f1,f2, end=' ')
+# for i in range(1, a):
+#     f1, f2 = f2, f1 + f2
+#     print(f2, end=' ')
+
+
+
+# a = [1, 12, 34, 1, 5, 67, 5, 3]
+# print(a)
+# b = set(a)
+# c = list(b)
+# print(c)
+
+# a = [1, 12, 34, 1, 5, 67, 5, 3]
+# print(a)
+# b = []
+# for i in a:
+#     if a.count(i) == 1:
+#         b.append(i)
+# print(b)
+
+# a = [1, 12, 34, 1, 5, 67, 5, 3]
+# print(a)
+# b = []
+# for i in a:
+#     if a.count(i) >= 2:
+#         a.remove(i)
+# b.append(i)
+# print(a)
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# c = []
+# print(a)
+# print(b)
+# for i in a:
+#     if i in b:
+#         c.append(i)
+# print(c)
+
+
+
